bbcbb5949d7e97475a0673bad548ef5ce7d1d95c
Cedric <cedric@cedricheisel.de>
2021-05-10 16:29:17 +0200

    init repo with approuter

---
+++ dummyUI5/webapp/controller/main.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/model/json/JSONModel',
+    'sap/m/MessageToast',
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller, JSONModel, MessageToast) {
+		"use strict";
+
+		return Controller.extend("dummyUI5.controller.main", {
+			onInit: function () {
+                //var oModel = new JSONModel("model/apps.json");
+                //this.getView().setModel(oModel);
+            },
+            onListItemPress: function (oEvent) {
+                MessageToast.show("Opening " + oEvent.getSource().getTitle());
+                var link2app = oEvent.getSource().getTooltip();
+                window.open(link2app,'_blank');
+		    }
+		});
+	});
